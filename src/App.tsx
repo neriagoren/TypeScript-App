db335f3745e53e87614ce9624dfe7d21fa80d374
neriagoren <neriatheman@gmail.com>
2022-07-03 07:43:04 +0300

    setting up a TypeScript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
+
+  const num: number = 10;
+  const bool: boolean = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
+      <h1 className=" text-center text-6xl font-bold text-pink-300">
+        Hello World!
+      </h1>
     </div>
+
   );
 }
 
